da4e322c93e40733fa793e52db50d300bd28da15
Vladimir Zdorenko <vovchikzd@gmail.com>
2023-10-15 16:33:26 +0300

    Just formatted

--- 2_day/part_one.rs
+++ 2_day/part_one.rs
@@ -10,32 +10,26 @@ fn main() {
         let enemy = clear_line.chars().nth(0).unwrap();
         let myne = clear_line.chars().nth(2).unwrap();
         match enemy {
-            'A' => {
-                match myne {
-                    'X' => score += 4,
-                    'Y' => score += 8,
-                    'Z' => score += 3,
-                    _   => unreachable!("You shouldn't be here"),
-                }
-            }
+            'A' => match myne {
+                'X' => score += 4,
+                'Y' => score += 8,
+                'Z' => score += 3,
+                _ => unreachable!("You shouldn't be here"),
+            },
 
-            'B' => {
-                match myne {
-                    'X' => score += 1,
-                    'Y' => score += 5,
-                    'Z' => score += 9,
-                    _   => unreachable!("You shouldn't be here"),
-                }
-            } 
+            'B' => match myne {
+                'X' => score += 1,
+                'Y' => score += 5,
+                'Z' => score += 9,
+                _ => unreachable!("You shouldn't be here"),
+            },
 
-            'C' => {
-                match myne {
-                    'X' => score += 7,
-                    'Y' => score += 2,
-                    'Z' => score += 6,
-                    _   => unreachable!("You shouldn't be here"),
-                }
-            } 
+            'C' => match myne {
+                'X' => score += 7,
+                'Y' => score += 2,
+                'Z' => score += 6,
+                _ => unreachable!("You shouldn't be here"),
+            },
 
             _ => unreachable!("You shouldn't be here"),
         }
--- 2_day/part_two.rs
+++ 2_day/part_two.rs
@@ -10,32 +10,26 @@ fn main() {
         let enemy = clear_line.chars().nth(0).unwrap();
         let myne = clear_line.chars().nth(2).unwrap();
         match enemy {
-            'A' => {
-                match myne {
-                    'X' => score += 3,
-                    'Y' => score += 4,
-                    'Z' => score += 8,
-                    _   => unreachable!("You shouldn't be here"),
-                }
-            }
+            'A' => match myne {
+                'X' => score += 3,
+                'Y' => score += 4,
+                'Z' => score += 8,
+                _ => unreachable!("You shouldn't be here"),
+            },
 
-            'B' => {
-                match myne {
-                    'X' => score += 1,
-                    'Y' => score += 5,
-                    'Z' => score += 9,
-                    _   => unreachable!("You shouldn't be here"),
-                }
-            } 
+            'B' => match myne {
+                'X' => score += 1,
+                'Y' => score += 5,
+                'Z' => score += 9,
+                _ => unreachable!("You shouldn't be here"),
+            },
 
-            'C' => {
-                match myne {
-                    'X' => score += 2,
-                    'Y' => score += 6,
-                    'Z' => score += 7,
-                    _   => unreachable!("You shouldn't be here"),
-                }
-            } 
+            'C' => match myne {
+                'X' => score += 2,
+                'Y' => score += 6,
+                'Z' => score += 7,
+                _ => unreachable!("You shouldn't be here"),
+            },
 
             _ => unreachable!("You shouldn't be here"),
         }
