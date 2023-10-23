27a610f06359a4bbb0b77682e26608bca00424cf
Vladimir Zdorenko <vovchikzd@gmail.com>
2023-10-23 08:06:18 +0300

    Do day six part one

---
+++ 6_day/part_one.rs
@@ -0,0 +1,26 @@
+use std::{io, io::BufRead};
+
+fn main() {
+    let mut readed = String::new();
+    let _ = io::stdin().lock().read_line(&mut readed);
+    let mut buffer: Vec<char> = readed.chars().collect();
+    buffer.pop();
+
+    let mut local_buffer: Vec<char> = Vec::with_capacity(4);
+    let mut counter = 0;
+
+    for ind in 0..buffer.len() {
+        while local_buffer.contains(&buffer[ind]) {
+            local_buffer.remove(0);
+        }
+
+        if local_buffer.len() != 4 {
+            local_buffer.push(buffer[ind]);
+        } else {
+            counter = ind;
+            break;
+        }
+    }
+
+    println!("{}", counter);
+}
